02d19d7287c95ddc5934f956d4a57295d3810027
frengkialfiansyah <and.frengky@gmail.com>
2023-07-03 15:12:39 +0700

    add handler mobile

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useDrag, DragPreviewImage } from "react-dnd";
+import { useDrag, DragPreviewImage, DndProvider } from "react-dnd";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 function Piece({ piece: { type, color }, position }) {
   const image = require(`./assets/${type}-${color}.png`);
@@ -13,19 +14,26 @@ function Piece({ piece: { type, color }, position }) {
 
   return (
     <>
-      <DragPreviewImage
-        connect={dragPreview}
-        src={image}
-        className="h-12 w-12"
-      />
-      <div
-        className={`${
-          isDragging ? "opacity-0" : "opacity-[1]"
-        } flex h-full w-full cursor-grab items-center justify-center`}
-      >
-        {/* {position} */}
-        <img src={image} alt="" className="max-h-full max-w-full" ref={drag} />
-      </div>
+      <DndProvider backend={TouchBackend}>
+        <DragPreviewImage
+          connect={dragPreview}
+          src={image}
+          className="h-12 w-12"
+        />
+        <div
+          className={`${
+            isDragging ? "opacity-0" : "opacity-[1]"
+          } flex h-full w-full cursor-grab items-center justify-center`}
+        >
+          {/* {position} */}
+          <img
+            src={image}
+            alt=""
+            className="max-h-full max-w-full"
+            ref={drag}
+          />
+        </div>
+      </DndProvider>
     </>
   );
 }
